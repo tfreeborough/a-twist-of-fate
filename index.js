4296b2d7c92e0b02db209bef6b30ac4a886716b7
root <root@dedivps-48025.dedicloud.co.uk>
2016-02-14 09:11:10 -0500

    Added socket.io

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(80);
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(80, function () {
-  console.log('Example app listening on port 80!');
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
