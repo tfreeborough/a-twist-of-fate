c31800085174561569b8320fe88438cb271c5cc3
AlexSalam <vornat2@gmail.com>
2016-03-06 23:51:05 +0000

    Socket.broadcast

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ var queueConnection = io.of('/queue').on('connection', function(socket) {
     	console.log("Queueing connection " + $id + ".");
     	io.of('/queue').emit('queueRequestAccepted', {id: $id, name: $data});
     	queue.push({id: $id, name: $data});
-    	io.of('/queue').broadcast.emit('queueClientCount', {connections: queue.length});
+    	socket.broadcast.emit('queueClientCount', {connections: queue.length});
     	startGames();
     });
 });
