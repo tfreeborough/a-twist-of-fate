7213ebd81f24d75853f644236589054e9e3ddb35
Thomas Freeborough <4newmusic@googlemail.com>
2016-03-06 23:14:52 +0000

    Send username with queue request

--- components/body/content/parts/queueTimer.js
+++ components/body/content/parts/queueTimer.js
@@ -2,11 +2,26 @@
 
 
 var QueueTimer = React.createClass({
+    getInitialState: function(){
+        return({
+            timeInQueue:0
+        })
+    },
+    componentWillReceiveProps: function(props){
+        if(this.props.inQueue == 'true') {
+            var that = this;
+            /*
+            var queueTiming = setInterval(function () {
+                that.setState({timeInQueue: (that.state.timeInQueue + 1)})
+            }, 1000);
+            */
+        }
+    },
     render: function(){
         if(this.props.inQueue == 'true'){
             return(
                 <div>
-                    In Queue
+                    Time in queue: {this.state.timeInQueue} seconds
                 </div>
             )
         }
--- components/body/content/playContent.js
+++ components/body/content/playContent.js
@@ -4,25 +4,41 @@ var PlayContent = React.createClass({
     getInitialState: function(){
         return({
             inQueue:'false',
-            queueId:''
+            queueId:'',
+            queueName:''
         })
     },
+    updateQueueName: function(event){
+        this.setState({queueName:event.target.value})
+    },
     requestQueue: function(){
-        var that = this;
-        queueSocket.emit('requestQueue');
-        queueSocket.on('queueRequestAccepted',function(data){
-            console.log('queueRequestAccepted received');
-            that.setState({inQueue:'true'});
-            that.setState({queueId:data.id});
-        });
+        if(this.state.queueName != '') {
+            var that = this;
+            queueSocket.emit('requestQueue', {username: this.state.queueName});
+            queueSocket.on('queueRequestAccepted', function (data) {
+                console.log('queueRequestAccepted received');
+                console.log(data);
+                that.setState({inQueue: 'true'});
+                that.setState({queueId: data.id});
+            });
+        }else{
+            alert('Please enter something for your username');
+        }
     },
     render: function(){
         return(
             <div>
-                <p className="flow-text">Whoa, so you wanna play a game buddy?</p>
-                <h4><a className="purple-text darken-2" href="javascript:void(0)" onClick={this.requestQueue}>Connect</a></h4>
+                <p className="flow-text">Please enter your name and then click 'Click to Queue'.</p>
+                <input type="text" onChange={this.updateQueueName} />
+                <span>Username: {this.state.queueName}</span>
+
+                <h4>
+                    <a className="purple-text darken-2" href="javascript:void(0)" onClick={this.requestQueue}>Click to Queue</a>
+                </h4>
+
                 <p className="flow-text">Connected to queue: {this.state.inQueue}</p>
                 <p className="flow-text">Queue Id: {this.state.queueId}</p>
+
                 <QueueTimer inQueue={this.state.inQueue} />
             </div>
         )
