35dd48998fa2e6a1aafca87ab37f68ab4d9d23fb
Thomas Freeborough <4newmusic@googlemail.com>
2016-03-06 22:10:41 +0000

    Added react component for basic connection events

--- components/body/content/playContent.js
+++ components/body/content/playContent.js
@@ -1,10 +1,28 @@
 /** @jsx React.DOM */
 
 var PlayContent = React.createClass({
+    getInitialState: function(){
+        return({
+            inQueue:'false',
+            queueId:''
+        })
+    },
+    requestQueue: function(){
+        var that = this;
+        socket.emit('requestQueue');
+        socket.on('queueRequestAccepted',function(data){
+            console.log('queueRequestAccepted received');
+            that.setState({inQueue:'true'});
+            that.setState({queueId:data.id});
+        });
+    },
     render: function(){
         return(
             <div>
                 <p className="flow-text">Whoa, so you wanna play a game buddy?</p>
+                <h4><a className="purple-text darken-2" href="javascript:void(0)" onClick={this.requestQueue}>Connect</a></h4>
+                <p className="flow-text">Connected to queue: {this.state.inQueue}</p>
+                <p className="flow-text">Queue Id: {this.state.queueId}</p>
             </div>
         )
     }
