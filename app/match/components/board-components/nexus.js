09e7ff82d27ce9db0dd45a8346be57a3028e386c
Thomas Freeborough <b2019162@my.shu.ac.uk>
2016-04-04 23:01:50 +0100

    Added nexus health and dynamic svg circle health meter effect that I so desperately wanted - Tom

--- app/match/components/board-components/nexus.js
+++ app/match/components/board-components/nexus.js
@@ -9,11 +9,19 @@ var Nexus = React.createClass({
             }
         )
     },
+    componentDidMount: function(){
+        var that = this;
+        setInterval(function(){
+            var random = Math.floor(Math.random() * 45) + 1;
+            that.setState({health:random});
+        },3000)
+    },
     render: function(){
        return(
            <div className="nexus-wrapper">
                <div className="nexus">
-                    <NexusImage id={this.props.id} />
+                   <NexusImage id={this.props.id} />
+                   <NexusHealth health={this.state.health} />
                </div>
            </div>
        )
